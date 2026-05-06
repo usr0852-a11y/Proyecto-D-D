76c17e59bd98d9ecc3ae2ef447edf001fec9d91a
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-06 15:04:25 +0100

    Añadí el constructor de la clase hija y llamé a los atributos de la clase padre con super

--- Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoNacional/PedidoNacional.java
+++ Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoNacional/PedidoNacional.java
@@ -5,5 +5,11 @@ import net.salesianoslacuesta.pedidoNacional.PedidoNacional;
 public class PedidoNacional extends Pedido {
     private double peso;
     private boolean urgente;
+
+    public PedidoNacional(String codigo, String cliente, double importeBase, double peso, boolean urgente) {
+        super(codigo, cliente, importeBase);
+        this.peso = peso;
+        this.urgente = urgente;
+    }
 }
 
