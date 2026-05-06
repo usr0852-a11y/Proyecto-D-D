d6316b6d1a04e494b7f29600963dc0c4decbd81b
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-06 16:00:11 +0100

    Añadí el constructor de la clase, llamando al super que es pedido y, además añadiendo los atributos de la clase pedido internacional

--- Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoInternacional/PedidoInternacional.java
+++ Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoInternacional/PedidoInternacional.java
@@ -6,5 +6,10 @@ import net.salesianoslacuesta.pedido.Pedido;
 public class PedidoInternacional extends Pedido {
     private String paisDestino;
     private boolean aduanas;
-    
+
+    public PedidoInternacional(String codigo, String cliente, double importeBase, String paisDestino, boolean aduanas) {
+        super(codigo, cliente, importeBase);
+        this.paisDestino = paisDestino;
+        this.aduanas = aduanas;
+    }
 }
