7b68d5a0a0226870978b3a89a2d77958ee004d3b
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-06 15:55:52 +0100

    Añadimos en la clase pedido internacional sus atributos

---
+++ Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoInternacional/PedidoInternacional.java
@@ -0,0 +1,10 @@
+package net.salesianoslacuesta.pedidoInternacional;
+
+import net.salesianoslacuesta.pedidoInternacional.PedidoInternacional;
+import net.salesianoslacuesta.pedido.Pedido;
+
+public class PedidoInternacional extends Pedido {
+    private String paisDestino;
+    private boolean aduanas;
+    
+}
