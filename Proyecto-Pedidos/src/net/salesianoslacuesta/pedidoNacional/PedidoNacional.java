474c346a290c4b0b49482956d1576413c59f7d6e
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-06 15:00:54 +0100

    Añadí los atributos de la clase pedido nacional

---
+++ Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoNacional/PedidoNacional.java
@@ -0,0 +1,9 @@
+package net.salesianoslacuesta.pedidoNacional;
+import net.salesianoslacuesta.pedido.Pedido;
+import net.salesianoslacuesta.pedidoNacional.PedidoNacional;
+
+public class PedidoNacional extends Pedido {
+    private double peso;
+    private boolean urgente;
+}
+
