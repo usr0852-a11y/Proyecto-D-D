d51d2053b1511c38382d28b98225c2da0d851fec
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-07 08:34:34 +0100

    Sobreescribí el método de calcular el importe final con los requisitos que pedía el ejercicio

--- Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoInternacional/PedidoInternacional.java
+++ Proyecto-Pedidos/src/net/salesianoslacuesta/pedidoInternacional/PedidoInternacional.java
@@ -34,5 +34,24 @@ public class PedidoInternacional extends Pedido {
     public void setAduanas(boolean aduanas) {
         this.aduanas = aduanas;
     }
-    
+
+    @Override
+    public double calcularImporteFinal() {
+        double total = getImporteBase();
+
+        total += 25;
+
+        if (aduanas = true) {
+            total += getImporteBase() * 0.12;
+        }
+
+        if(paisDestino.equals("Suiza") || paisDestino.equals("Noruega") ) {
+            total += 18;
+        }
+
+        if (getImporteBase() > 200) {
+            total -= 10;
+        }
+        return total;
+    }
 }
