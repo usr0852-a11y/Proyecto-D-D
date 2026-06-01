74b67528fea32db7b537ac10833bd6101f482d8f
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-06-01 09:38:32 +0100

    Añadí los métodos de entrenar, descansar, gastar energía, mostrar informacion y mostrar el total de personajes

--- Proyecto-D-D/src/net/salesianoslacuesta/personaje/Personaje.java
+++ Proyecto-D-D/src/net/salesianoslacuesta/personaje/Personaje.java
@@ -1,7 +1,7 @@
 package net.salesianoslacuesta.personaje;
 
 public class Personaje  {
-   private String nombre;
+    private String nombre;
     private int nivel;
     private int energia;
 
@@ -39,6 +39,44 @@ public class Personaje  {
     public void setEnergia(int energia) {
         this.energia = energia;
     }
+    public void entrenar(int x) {
+        if(x <= 0) {
+            System.out.println("No se puede entrenar 0 o menos niveles.");
+        
+        } else {
+            this.nivel += x;
+        }
+    }
+
+    public void descansar(int x) {
+        if(x <= 0) {
+            System.out.println("No se puede aumentar 0 o menos energia");
+        } else if (energia + x > 100){
+        System.out.println("La energia no puede superar los 100");
+        energia = 100;
+        } else {
+            energia += x;
+        }
+    }
+
+    public void gastarEnergia(int x) {
+        energia -= x;
+        if (energia < 0 ) {
+            energia = 0;
+        }
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Nivel: " + nivel);
+        System.out.println("Energia: " + energia);
+
+    }
+
+    public static void mostrarTotalPersonajes() {
+        System.out.println("Total de personajes: " + totalPersonajes);
+    }
+
 
 
 }
