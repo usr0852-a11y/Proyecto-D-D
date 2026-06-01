efd5e53213132fb5a1d1aba615afd118b4e0f8ff
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-06-01 09:58:01 +0100

    Añadí un personaje que entrenara hasta el nivel 4, gastara 30 de energía y se mostrara su información y luego el total de personajes

--- Proyecto-D-D/src/App.java
+++ Proyecto-D-D/src/App.java
@@ -1,5 +1,16 @@
+import net.salesianoslacuesta.guerrero.Guerrero;
+import net.salesianoslacuesta.mago.Mago;
+import net.salesianoslacuesta.personaje.Personaje;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         Personaje viajero = new Personaje("Eldrin", 3);
+
+        viajero.entrenar(4);
+        viajero.gastarEnergia(30);
+        viajero.mostrarInfo();
+
+        Personaje.mostrarTotalPersonajes();
+        
     }
 }
