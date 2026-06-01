c27d039d4e5125b722d6ea672b4b4ca91efec4c6
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-06-01 09:32:10 +0100

    Añadí los atributos y el constructor de la clase personaje, comenzando con 100 de energia

--- Proyecto-D-D/src/net/salesianoslacuesta/personaje/Personaje.java
+++ Proyecto-D-D/src/net/salesianoslacuesta/personaje/Personaje.java
@@ -1,5 +1,18 @@
 package net.salesianoslacuesta.personaje;
 
-public class Personaje {
-    
+public class Personaje  {
+   private String nombre;
+    private int nivel;
+    private int energia;
+
+    private static int totalPersonajes;
+
+    public Personaje(String nombre, int nivel) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.energia = 100;
+        totalPersonajes++;
+    } 
 }
+   
+
