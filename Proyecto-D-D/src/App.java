ea73f10d6eade3e47c2f3e11a827e6200916b115
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-06-01 10:11:45 +0100

    Esta vez si guardo app

--- Proyecto-D-D/src/App.java
+++ Proyecto-D-D/src/App.java
@@ -1,5 +1,43 @@
+import net.salesianoslacuesta.guerrero.Guerrero;
+import net.salesianoslacuesta.mago.Mago;
+import net.salesianoslacuesta.personaje.Personaje;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         Personaje viajero = new Personaje("Eldrin", 3);
+
+        viajero.entrenar(4);
+        viajero.gastarEnergia(30);
+        viajero.mostrarInfo();
+
+        Personaje.mostrarTotalPersonajes();
+
+        System.out.println("-----------------------"); //separar personajes q se vea bien en consola
+
+        Guerrero guerrero1 = new Guerrero("Thorak", 5, "Hacha doble");
+        guerrero1.ataquePesado();
+        guerrero1.descansar(10);
+        guerrero1.mostrarInfo();
+        Personaje.mostrarTotalPersonajes();
+
+        System.out.println("-----------------------"); 
+
+        Mago mago1 = new Mago("Lyra", 4, "Hielo");
+
+        mago1.ataqueElemental();
+        mago1.entrenar(2);
+        mago1.mostrarInfo();
+        Personaje.mostrarTotalPersonajes();
+
+        System.out.println("----------------------------");
+
+        Personaje[] reino = {viajero, guerrero1, mago1};
+
+        for (int i = 0; i < reino.length ; i++) {
+            reino[i].mostrarInfo();
+
+        }
+
+
     }
 }
