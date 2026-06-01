25efb03bd31ba688671712c660131e168a61606b
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-06-01 10:13:13 +0100

    Añadí un personaje, viajero un guerrero y un mago usando las funciones que teníamos en las clases y además metí los tres personajes en un array reino y que se mostrara su información

--- Proyecto-D-D/src/App.java
+++ Proyecto-D-D/src/App.java
@@ -4,7 +4,7 @@ import net.salesianoslacuesta.personaje.Personaje;
 
 public class App {
     public static void main(String[] args) throws Exception {
-         Personaje viajero = new Personaje("Eldrin", 3);
+        Personaje viajero = new Personaje("Eldrin", 3);
 
         viajero.entrenar(4);
         viajero.gastarEnergia(30);
